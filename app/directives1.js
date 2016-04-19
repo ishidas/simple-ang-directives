bd7e6bd47afeb06bf5a0012ba5a091be141931f7
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-18 21:52:44 -0700

    added navbar.html, moved files around, added a directive

---
+++ app/directives1.js
@@ -0,0 +1,13 @@
+(function(angular){
+  var app = angular.module('myApp');
+
+  app.directives(function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/navbar.js'
+    };
+  });
+
+
+
+})();
